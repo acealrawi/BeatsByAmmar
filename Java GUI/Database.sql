55b8ad6b7931a101e28730595fb2464b429793b5
Faalhaaz <accvoorwarrock@gmail.com>
2017-06-02 14:50:43 +0200

    Updated Database
    So that all info goes in

--- Java GUI/Database.sql
+++ Java GUI/Database.sql
@@ -2,8 +2,14 @@ CREATE DATABASE heartinfo;
 
 USE heartinfo;
 
-CREATE TABLE heart(
-	timee date,
-    heartrate int
+CREATE TABLE userinfo(
+    timee date,
+    heartrate int,
+    name varchar(50),
+    gender char(1),
+    age int,
+    patientnr int,
+    weight int,
+    sport varchar(50)
 );
     
