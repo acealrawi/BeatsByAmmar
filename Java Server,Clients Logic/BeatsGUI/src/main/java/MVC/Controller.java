da561c8034d04cb44c2694c9565f096726699574
Ali Alrawi <alialrawiu@gmail.com>
2017-06-12 11:21:40 +0200

    added a gear button

--- Java Server,Clients Logic/BeatsGUI/src/main/java/MVC/Controller.java
+++ Java Server,Clients Logic/BeatsGUI/src/main/java/MVC/Controller.java
@@ -8,22 +8,54 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
 import javafx.stage.Stage;
 import proto.ProtoPatientOuterClass;
 import Client.AbstractDataHandler;
+
+import javax.net.ssl.HostnameVerifier;
+import java.lang.reflect.GenericArrayType;
 
 
 public class Controller extends AbstractDataHandler {
 
     View view = new View();
+    int x = 0;
 
     @FXML
     TextField NumberInput;
 
+    @FXML
+    Button settingButton;
 
 
+    public void changeSetting(){
 
+        if(x==0){
+            settingButton.setStyle("-fx-background-image: url('CSS/hover.png')");
+            x = 1;
+        }else if(x == 1){
+            settingButton.setStyle("-fx-background-image: url('CSS/gear.png')");
+            x = 0;
+        }
+    }
+
+    public void handleSetting(){
+        try{
+            Parent secondParent = FXMLLoader.load(getClass().getClassLoader().getResource("FxmlScreens/SettingsScreen.fxml"));
+            Stage secondStage = view.getStage();
+
+            secondStage.setScene(new Scene(secondParent, 360, 180));
+            secondStage.setTitle("Saxion hospital");
+            secondStage.setResizable(false);
+            secondStage.show();
+        }catch (Exception io){
+            io.printStackTrace();
+        }
+    }
 
 
 
